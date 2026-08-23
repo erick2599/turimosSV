5509236ca96f16d7483ef57c776e8f464c1b0bcb
Erick Gaitan <59844595+erick2599@users.noreply.github.com>
2026-08-23 15:58:53 -0600

    Creo que este si es el final

--- app/Models/Lugar.php
+++ app/Models/Lugar.php
@@ -9,7 +9,7 @@ class Lugar
         // Usamos base_path para apuntar directamente a la carpeta database
         $path = base_path('database/lugares.json');
 
-        if (!file_exists($path)) {
+        if (! file_exists($path)) {
             return []; // Retorna vacío si el archivo no está ahí
         }
 
@@ -26,6 +26,7 @@ class Lugar
                 return $lugar;
             }
         }
+
         return null;
     }
 }
