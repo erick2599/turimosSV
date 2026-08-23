514682ee37d34494687fbab72f8d1fdcf3e7a304
Erick Gaitan <59844595+erick2599@users.noreply.github.com>
2026-08-23 16:30:35 -0600

    Creo que este si es el final...ya perdi la esperanza

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LugarController::class, 'index'])->name('lugares.index');
 Route::get('/lugar/{id}', [LugarController::class, 'show'])->name('lugares.show');
 Route::post('/contacto', [LugarController::class, 'contactar'])->name('lugares.contacto');
+Route::get('/settings/profile', function () {
+    return 'profile';
+})->name('profile.edit');
+
+Route::get('/settings/security', function () {
+    return 'security';
+})->name('security.edit');
